5e810cf901c9edddd47c2899b70bf78b08a1893a
Kevin Wu <kevin@kevinniuwu.com>
2021-03-06 18:32:20 -0500

    Implement token authentication for workspace views

--- workspaces/serializers.py
+++ workspaces/serializers.py
@@ -1,9 +1,23 @@
 from rest_framework import serializers
+from django.utils.translation import gettext_lazy as _
 
 from .models import Workspace
 
 
 class WorkspaceSerializer(serializers.ModelSerializer):
+    # separate field for password for workspace user password
+    password = serializers.CharField(
+        label=_("Password"),
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
+
     class Meta:
         model = Workspace
-        fields = ('id', 'unique_id', 'created_at')
+        fields = ('id', 'unique_id', 'nickname', 'created_at', 'anonymous_readable', 'password')
+
+    def create(self, validated_data):
+        # override default create method to use custom workspace manager create
+        workspace = Workspace.objects.create_workspace(**validated_data)
+        return workspace
--- workspaces/views.py
+++ workspaces/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse, JsonResponse
-from django.core import serializers
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
 from .models import Workspace
-from .forms import CreateWorkspaceForm
 from .serializers import WorkspaceSerializer
+from .permissions import IsReadableOrAuthenticated
 
 
 class WorkspaceCreateView(generics.CreateAPIView):
@@ -13,41 +15,45 @@ class WorkspaceCreateView(generics.CreateAPIView):
     serializer_class = WorkspaceSerializer
 
 
+# TODO: if readable but not authed, then return read-only links to the workspace apps
 class WorkspaceRetrieveView(generics.RetrieveAPIView):
+    permission_classes = [IsReadableOrAuthenticated]
     queryset = Workspace.objects.all()
     serializer_class = WorkspaceSerializer
     lookup_field = 'unique_id'
 
 
-def main(request):
-    if request.method != 'GET':
-        return HttpResponse(status=405)
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        if 'unique_id' not in data:
+            raise ValidationError('Must include "unique_id".',
+                                  code='authorization')
 
-    context = {
-        'form': CreateWorkspaceForm()
-    }
-    return HttpResponse(render(request, 'workspace/index.html', context))
+        try:
+            workspace = Workspace.objects.get(unique_id=data['unique_id'])
+        except ObjectDoesNotExist:
+            raise ValidationError('Workspace with given "unique_id" does not exist.',
+                                  code='authorization')
 
+        if workspace.user is None:
+            raise ValidationError('Workspace is globally editable',
+                                  code='authorization')
 
-def create_workspace(request):
-    # respond to workspace creation form
-    if request.method != 'POST':
-        return HttpResponse(status=405)
+        # since username is the unique_id and the user serializer class expects the username field,
+        # reassign the unique_id field
+        updated_data = data.copy()  # copy since request.data is immutable
+        updated_data['username'] = updated_data['unique_id']
 
-    form = CreateWorkspaceForm(request.POST)
-    if form.is_valid():
-        workspace = Workspace()
-        workspace.save()
+        serializer = self.serializer_class(data=updated_data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
 
-        return redirect('get-workspace', workspace.unique_id)
-    else:
-        return redirect('main')
+        assert user.workspace == workspace
 
-
-def get_workspace(request, unique_id):
-    if request.method != 'GET':
-        return HttpResponse(status=405)
-
-    workspace = get_object_or_404(Workspace, unique_id=unique_id)
-    workspace_json = serializers.serialize('json', [workspace])
-    return JsonResponse(workspace_json, safe=False)
+        return Response({
+            'token': token.key,
+            'unique_id': user.workspace.unique_id
+        })
