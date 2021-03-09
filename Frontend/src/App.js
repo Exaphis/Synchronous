d9d5a1d03af5b0227514f9e205833e72be47efc7
rborzell <rborzell@purdue.edu>
2021-03-08 19:08:06 -0600

    Changed Create UI

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,10 +7,13 @@ import {
   Switch,
   Route,
   Link,
+  useRouteMatch,
+  useParams,
+  withRouter
 } from "react-router-dom";
 
 import ReactDOM from 'react-dom'
-//import rnd, { Rnd } from 'react-rnd'
+import rnd, { Rnd } from 'react-rnd'
 import TextareaAutosize from 'react-textarea-autosize';
 import Draggable, {DraggableCore} from 'react-draggable'; // Both at the same time
 
@@ -20,13 +23,13 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-//import FormControlLabel from '@material-ui/core/FormControlLabel';
-//import Checkbox from '@material-ui/core/Checkbox';
-//import {Link as uiLink} from '@material-ui/core/Link';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import {Link as uiLink} from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-//import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-//import AddIcon from '@material-ui/icons/Add'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import AddIcon from '@material-ui/icons/Add'
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -34,32 +37,29 @@ import Container from '@material-ui/core/Container';
 
 
 export default function App() {
-    return (
-        <Router>
-          <div>
-            <Switch>
-              <Route exact path="/Create">
-                <Create/>
-              </Route>
-              <Route exact path="/Open">
-                <Open/>
-              </Route>
-              <Route exact path="/Upload">
-                <Upload/>
-              </Route>
-              <Route exact path="/">
-                <SignIn/>
-              </Route>
-              <Route exact path="/Test">
-                <Test/>
-              </Route>
-              <Route exact path="/Email">
-                <Email/>
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-    );
+  return (
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/Create">
+              <Create/>
+            </Route>
+            <Route exact path="/Open">
+              <Open/>
+            </Route>
+            <Route exact path="/Upload">
+              <Upload/>
+            </Route>
+            <Route exact path="/">
+              <SignIn/>
+            </Route>
+            <Route exact path="/Test">
+              <Test/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+  );
 }
 
 function SignIn() {
@@ -69,7 +69,7 @@ function SignIn() {
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <div className={classes.paper}>
-          <img alt="" src={logo} width="400" height="400"/>
+          <img src={logo} width="400" height="400"/>
           <form className={classes.form} noValidate>
             <Router>
               <div>
@@ -121,18 +121,6 @@ function SignIn() {
                 >
                   Test Workspace
                 </Button>
-                <Button
-                    component={ Link }
-                    to={"/Email"}
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    //color="primary"
-                    className={classes.submit}
-                    onClick={ refresh }
-                >
-                  Test Email
-                </Button>
               </div>
             </Router>
           </form>
@@ -145,9 +133,6 @@ function SignIn() {
 }
 
 function Copyright() {
-  /*<Link color="inherit" href="https://material-ui.com/">
-    Synchronous
-  </Link>{' '}*/
   return (
       <Typography variant="body2" color="textSecondary" align="center">
         {'Copyright © Synchronous '}
@@ -163,14 +148,14 @@ function Copyright() {
 
 function Test() {
   return (
-    ReactDOM.render(
-     <Draggable>
-       <div> <TextareaAutosize/></div>
-     </Draggable>, document.getElementById('root')
-  )
+      ReactDOM.render(
+          <Draggable>
+            <div> <TextareaAutosize/></div>
+          </Draggable>, document.getElementById('root')
+      )
   )
 }
-  
+
 
 function Create() {
   const classes = useStyles();
@@ -190,7 +175,7 @@ function Create() {
                 variant="outlined"
                 margin="normal"
                 fullWidth
-                id="name"
+                id="email"
                 label="Workspace Name"
                 name="workspace"
                 autoComplete="workspace"
@@ -204,20 +189,21 @@ function Create() {
                 id="password"
                 label="Password (Optional)"
                 name="workspace"
+                type="password"
                 autoComplete="workspace"
             />
             <Box mt={2}>
             </Box>
             <Button
                 size="large"
-                //component={ Link }
-                //to={"/Test"}
+                component={ Link }
+                to={"/Test"}
                 type="submit"
                 fullWidth
                 variant="contained"
                 color="primary"
                 className={classes.submit}
-                onClick={ () => handleCreate(document.getElementById('name'), document.getElementById('password')) }
+                onClick={ refresh }
             >
               Create Workspace
             </Button>
@@ -258,7 +244,7 @@ function Open() {
                 variant="outlined"
                 margin="normal"
                 fullWidth
-                id="email"
+                id="name"
                 label="Workspace Name"
                 name="workspace"
                 autoComplete="workspace"
@@ -297,21 +283,6 @@ function Open() {
 
 function Upload() {
   return <h2>Upload: TODO</h2>
-}
-
-function Email() {
-  return <h2>Email: TODO</h2>
-}
-
-function handleCreate(name, password) {
-  console.log("Workspace name is: ");
-  console.log(name.value);
-  alert(name.value)
-  console.log("\nWorkspace password is: ");
-  //console.log(password)
-  alert(password.value)
-
-  return <TextField id="name" label="Erro" />
 }
 
 
