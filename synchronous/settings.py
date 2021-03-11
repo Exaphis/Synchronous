a78d367514d52d8348c235199d1b11a1827072d4
Saksham Jain <saksham23@gmail.com>
2021-03-11 02:35:16 -0500

    email functionality added
    yet to test

--- synchronous/settings.py
+++ synchronous/settings.py
@@ -154,3 +154,13 @@ CELERYBEAT_SCHEDULE = {
         'schedule': 1
     },
 }
+
+
+#Email settings for auto delete
+
+MAIL_BACKEND =‘django.core.mail.backends.smtp.EmailBackend
+EMAIL_HOST = ‘smtp.gmail.com’
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ‘synchronous307@gmail.com’
+EMAIL_HOST_PASSWORD = ‘Teslasucks’
--- workspaces/tasks.py
+++ workspaces/tasks.py
@@ -1,5 +1,7 @@
 from django.utils import timezone
-from django.core.mail import EmailMessage
+# from django.core.mail import EmailMessage
+
+from inform_using_mail import send_mail_to
 
 from .models import Workspace
 
@@ -19,14 +21,17 @@ def delete_old_workspace():
         if w.expiration_date < timezone.now():
             w.delete()
             # log deletion
-
-        # email = EmailMessage(
-        # 'Workspace ' + w.unique_id + 'was deleted',
-        # 'Your 24 hours for your workspace were up! hope you saved your progress :)',
-        # 'synchronous307@gmail.com',
-        # ['sakshamj23@gmail.com'],
-        # reply_to=['synchronous307@gmail.com'],
-        # headers={'Message-ID': 'Synchronous'},
-        # )
+        else if w.expiration_date > timezone.now and w.expiration_date < timezone.now + timezone.timedelta(hours=1)
+            # email = EmailMessage(
+            # 'Workspace ' + w.unique_id + 'will be deleted in an hour',
+            # 'Your 24 hours for your workspace are about to be up! Save your progress! :)',
+            # 'synchronous307@gmail.com',
+            # ['sakshamj23@gmail.com'],
+            # fail_silently=False
+            # )
+            subject= 'Workspace ' + w.unique_id + 'will be deleted in an hour'
+            message= 'The 24 hours for your workspace are about to be up! Save your progress! :)'
+            receiver= 'sakshamj23l@gmail.com'
+            send_mail_to(subject,message,receivers)
 
     return "completed deleting workspaces at {}".format(timezone.now())
