b11eb8ed6c4754c5e481c1d905c314519c601c19
Shreyas Iyengar <32004803+pinkslp@users.noreply.github.com>
2021-03-29 14:55:58 -0400

    replaced filestack with uppy, picker is rendering but no successful upload.

--- Frontend/src/WorkspaceArea.js
+++ Frontend/src/WorkspaceArea.js
@@ -7,9 +7,11 @@ import * as rps from "react-pro-sidebar";
 import AddIcon from "@material-ui/icons/Add";
 import CloseIcon from '@material-ui/icons/Close';
 import MinimizeIcon from '@material-ui/icons/Minimize';
-import * as filestack from 'filestack-js';
-import {PickerInline, PickerOverlay} from 'filestack-react';
-const client = filestack.init('ABFJfmCStQGmS8LZG3Miez');
+
+import Uppy from '@uppy/core'
+import Tus from '@uppy/tus'
+import { DragDrop } from '@uppy/react'
+import store from '@uppy/store-default'
 
 
 function AppTitleBar(props) {
@@ -35,6 +37,43 @@ function AppTitleBar(props) {
 
 
 function WorkspaceApp(props) {
+    const uppy = new Uppy({
+        meta: { type: 'avatar' },
+        restrictions: { maxNumberOfFiles: 1 },
+        autoProceed: true
+      })
+      
+      uppy.use(Tus, { endpoint: '/upload' })
+      
+      uppy.on('complete', (result) => {
+        const url = result.successful[0].uploadURL
+        store.dispatch({
+          type: 'SET_USER_AVATAR_URL',
+          payload: { url: url }
+        })
+      })
+      
+      const AvatarPicker = ({ currentAvatar }) => {
+        return (
+          <div>
+            <img src={currentAvatar} alt="Current Avatar" />
+            <DragDrop
+              uppy={uppy}
+              locale={{
+                strings: {
+                  // Text to show on the droppable area.
+                  // `%{browse}` is replaced with a link that opens the system file selection dialog.
+                  dropHereOr: 'Drop here or %{browse}',
+                  // Used as the label for the link that opens the system file selection dialog.
+                  browse: 'browse'
+                }
+              }}
+            />
+          </div>
+        )
+      }
+
+
     return (
         <div id={props.uuid} style={{
             backgroundColor: 'white',
@@ -48,10 +87,8 @@ function WorkspaceApp(props) {
                          onMinimize={props.onMinimize}/>
             <iframe style={{flexGrow: 1, display: props.minimized ? 'none' : 'block'}}
                     title={props.uuid}/>
-                    <PickerInline
-                        apikey={'ABFJfmCStQGmS8LZG3Miez'}
-                        onSuccess={(res) => console.log("result" + JSON.stringify(res))}
-                    />
+                    <AvatarPicker></AvatarPicker>
+                    
         </div>
     )
 }
