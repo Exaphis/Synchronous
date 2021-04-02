1f4bd54fc4dbe694c4eaec284d1c19f08a330ffc
Kevin Wu <kevin@kevinniuwu.com>
2021-04-02 10:27:37 -0400

    Add alt text, collapse empty tags

--- Frontend/src/Tutorial.js
+++ Frontend/src/Tutorial.js
@@ -73,7 +73,7 @@ function Introduction() {
             <h1>
                 Introduction:
             </h1>
-            <p class="tutorial-p"> Synchronous is positioned as a real time collaboration tool, with the ability to create virtual workspaces for teams, and offering tools and services inside the workspace that will allow for better communication and collaboration, such as a virtual whiteboard, notepad as well as file sharing, all without having to set up an account or register with the website. We envisioned it as bridging the gap between Zoom and the Google suite of applications, bringing security as well as flexibility all without the overhead of storing information or creating an account. </p>
+            <p className="tutorial-p"> Synchronous is positioned as a real time collaboration tool, with the ability to create virtual workspaces for teams, and offering tools and services inside the workspace that will allow for better communication and collaboration, such as a virtual whiteboard, notepad as well as file sharing, all without having to set up an account or register with the website. We envisioned it as bridging the gap between Zoom and the Google suite of applications, bringing security as well as flexibility all without the overhead of storing information or creating an account. </p>
         </div>
 
     );
@@ -85,20 +85,20 @@ function Create() {
             <h1>
                 Creating a Workspace:
             </h1>
-            <p class="tutorial-p">To create a workspace follow these steps: </p>
-            <br></br>
-                <p class="steps">
+            <p className="tutorial-p">To create a workspace follow these steps: </p>
+            <br />
+                <p className="steps">
                     Step 1: Click on "Start a new workspace" in the main menu.
                 </p>
-                <img src={cstep1} class="step-image"></img>
-                <p class="steps">
+                <img src={cstep1} alt="Step 1 screenshot" className="step-image"/>
+                <p className="steps">
                     Step 2: Enter your workspace name or check "Use Id?" and enter your workspace ID along with password (if set) and click on "Okay".
                 </p>
-                <img src={cstep2} class="step-image"></img>
-                <p class="steps">
+                <img src={cstep2} alt="Step 2 screenshot" className="step-image"/>
+                <p className="steps">
                     Your workspace is now created! Get ready for efficiency.
                 </p>
-                <img src={cstep3} class="step-image"></img>
+                <img src={cstep3} alt="Step 3 screenshot" className="step-image"/>
         </div>
 
     );
@@ -110,20 +110,20 @@ function Open() {
             <h1>
                 Opening an Existing Workspace:
             </h1>
-            <p class="tutorial-p">To open a workspace follow these steps: </p>
-            <br></br>
-                <p class="steps">
+            <p className="tutorial-p">To open a workspace follow these steps: </p>
+            <br />
+                <p className="steps">
                     Step 1: Click on "Reopen an existing workspace" in the main menu.
                 </p>
-                <img src={rostep1} class="step-image"></img>
-                <p class="steps">
+                <img src={rostep1} alt="Step 1 screenshot" className="step-image"/>
+                <p className="steps">
                     Step 2: Either click on "Create Workspace" right away to create a workspace with no custom name, password and accessible by all. Else, fill in the relavent details and click on create workspace.
                 </p>
-                <img src={rostep2} class="step-image"></img>
-                <p class="steps">
+                <img src={rostep2} alt="Step 2 screenshot" className="step-image"/>
+                <p className="steps">
                     Your workspace is now opened! Get back to working.
                 </p>
-                <img src={rostep3} class="step-image"></img>
+                <img src={rostep3} alt="Step 3 screenshot" className="step-image"/>
 
         </div>
 
@@ -136,88 +136,88 @@ function Workspace() {
             <h1>
                 Using your Workspace:
             </h1>
-            <p class="tutorial-p"> Click on a topic to know more</p>
-            <br></br>
-            <label class="collapsible">
+            <p className="tutorial-p"> Click on a topic to know more</p>
+            <br />
+            <label className="collapsible">
             <input type="checkbox" />
-            <span class="collapser">Change workspace nickname</span>
+            <span className="collapser">Change workspace nickname</span>
             
-            <div class="collapsed">
+            <div className="collapsed">
                 Step 1: Click on "Change Nickname" button
 
-                <img src={cnickstep1} class="step-image-inside" ></img>
+                <img src={cnickstep1} alt="Step 1 screenshot" className="step-image-inside" />
 
                 Step 2: Type in the nick name and click on "Okay"
 
-                <img src={cnickstep2} class="step-image-inside" ></img>
+                <img src={cnickstep2} alt="Step 2 screenshot" className="step-image-inside" />
 
                 The nick name of the workspace has now changed!
 
-                <img src={cnickstep3} class="step-image-inside" ></img>
+                <img src={cnickstep3} alt="Step 3 screenshot" className="step-image-inside" />
             
         
             </div>
             </label>
 
-            <label class="collapsible">
+            <label className="collapsible">
             <input type="checkbox" />
             
-            <div class="collapser">
+            <div className="collapser">
                 Invite collaborators
             </div>
-            <div class="collapsed">
+            <div className="collapsed">
                 Step 1: Click on the mail icon button
 
-                <img src={invstep1} class="step-image-inside" ></img>
+                <img src={invstep1} alt="Step 1 screenshot" className="step-image-inside" />
 
                 Step 2: Type in the email of the collaborator. You may add a message. Click on "Submit"
 
-                <img src={invstep2} class="step-image-inside" ></img>
+                <img src={invstep2} alt="Step 2 screenshot" className="step-image-inside" />
 
                 The collaborator has been sent an email with the details of this workspace. Sit tight, help is coming!
         
             </div>
             </label>
 
-            <label class="collapsible">
+            <label className="collapsible">
             <input type="checkbox" />
             
-            <div class="collapser">
+            <div className="collapser">
             Submit a bug / contact us
             </div>
-            <div class="collapsed">
+            <div className="collapsed">
                 Step 1: Click on the conversation bubble icon on the bottom left
 
-                <img src={bugstep1} class="step-image-inside" ></img>
+                <img src={bugstep1} alt="Step 1 screenshot" className="step-image-inside" />
 
-                Step 2: To leave us a message, type your message in the text box on the bottom left and hit enter <br></br>
+                Step 2: To leave us a message, type your message in the text box on the bottom left and hit enter <br />
                 To send a bug report, copy paste the Google form link in the browser in a new window.
-                <img src={bugstep2} class="step-image-inside" ></img>
+                <img src={bugstep2} alt="Step 2 screenshot" className="step-image-inside" />
 
                 Step3: Enter your bug details in the Google form and relax, as we take care of it!
 
-                <img src={bugstep3} class="step-image-inside" ></img>
+                <img src={bugstep3} alt="Step 3 screenshot" className="step-image-inside" />
             
         
             </div>
             </label>
 
-            <label class="collapsible">
+            <label className="collapsible">
             <input type="checkbox" />
-            <span class="collapser">Change your name</span>
+            <span className="collapser">Change your name</span>
             
-            <div class="collapsed">
+            <div className="collapsed">
                 Step 1: Click on "Change name" button
 
-                <img src={cnamestep1} class="step-image-inside" ></img>
+                <img src={cnamestep1} alt="Step 1 screenshot" className="step-image-inside" />
 
                 Step 2: Type in the name and click on "Change name"
 
-                <img src={cnamestep2} class="step-image-inside" ></img>
+                <img src={cnamestep2} alt="Step 2 screenshot" className="step-image-inside" />
 
                 Your name has now changed!
 
-                <img src={cnamestep3} class="step-image-inside" ></img>
+                <img src={cnamestep3} alt="Step 3 screenshot" className="step-image-inside" />
             
         
             </div>
@@ -235,99 +235,99 @@ function Apps() {
                 Available Apps:
             </h1>
 
-            <p class="tutorial-p"> Click on a topic to know more</p>
+            <p className="tutorial-p"> Click on a topic to know more</p>
 
-            <p class="tutorial-p"> How to handle Apps</p>
+            <p className="tutorial-p"> How to handle Apps</p>
             
-            <label class="collapsible">
+            <label className="collapsible">
             <input type="checkbox" />
-            <span class="collapser">Create new App</span>
+            <span className="collapser">Create new App</span>
             
-            <div class="collapsed">
-            Step 1: Click on + button to add a new tab <br></br>
+            <div className="collapsed">
+            Step 1: Click on + button to add a new tab <br />
 
             Step 2: Select the app you wish to add
 
-            <img src={createapp} class="step-image-inside" ></img>
+            <img src={createapp} alt="Step 2 screenshot" className="step-image-inside" />
             
         
             </div>
             </label>
 
-            <label class="collapsible">
+            <label className="collapsible">
             <input type="checkbox" />
             
-            <div class="collapser">
+            <div className="collapser">
                 Move and resize app
             </div>
-            <div class="collapsed">
-            Move: Click on the App and move it around <br></br>
+            <div className="collapsed">
+            Move: Click on the App and move it around <br />
 
             Resize: Hold and drag from the side you wish to resize
 
-            <img src={moveresize} class="step-image-inside" ></img>
+            <img src={moveresize} alt="Step 1 screenshot" className="step-image-inside" />
             </div>
             </label>
 
-            <label class="collapsible">
+            <label className="collapsible">
             <input type="checkbox" />
             
-            <div class="collapser">
+            <div className="collapser">
                 Minimize and maximize </div>
 
-                <div class="collapsed">
-                    Minimize: Click on the - icon on the top right of the app <br></br>
+                <div className="collapsed">
+                    Minimize: Click on the - icon on the top right of the app <br />
 
                     Maximize: Click on the minimized app from the sidebar 
 
-                    <img src={minimize} class="step-image-inside" ></img>
+                    <img src={minimize} alt="Step 1 screenshot" className="step-image-inside" />
             </div>
 
 
             </label>
 
-            <br></br>
+            <br />
 
-            <p class="tutorial-p"> How to use specific Apps</p>
+            <p className="tutorial-p"> How to use specific Apps</p>
             
-            <label class="collapsible">
+            <label className="collapsible">
             <input type="checkbox" />
-            <span class="collapser">Text editor</span>
+            <span className="collapser">Text editor</span>
             
-            <div class="collapsed">
+            <div className="collapsed">
             The text editor app is similar to any rich text editor you might be used to using. 
             To use simply add it from the side bar.
 
-            {/*<img src={createapp} class="step-image-inside" ></img>*/}
+            {/*<img src={createapp} className="step-image-inside" />*/}
             
         
             </div>
             </label>
 
-            <label class="collapsible">
+            <label className="collapsible">
             <input type="checkbox" />
             
-            <div class="collapser">
+            <div className="collapser">
                 Chat
             </div>
-            <div class="collapsed">
+            <div className="collapsed">
             Chat app can be used to chat with other collaborators on the work space. <br>
             </br>To access it, simply click on the chat icon on the bottom right of the workspace window and start typing your message!
 
-            <img src={chatstep} class="step-image-inside" ></img>
+            <img src={chatstep} alt="Step 1 screenshot" className="step-image-inside" />
             </div>
             </label>
 
-            <label class="collapsible">
+            <label className="collapsible">
             <input type="checkbox" />
             
-            <div class="collapser">
+            <div className="collapser">
                 File Sharing </div>
 
-                <div class="collapsed">
+                <div className="collapsed">
                     #TODO
 
-                     {/*<img src={createapp} class="step-image-inside" ></img>*/}
+                     {/*<img src={createapp} className="step-image-inside" />*/}
             </div>
 
 
